256403816f6422f08aa7d3b0509b3f35c1826443
Lean <lean.li@71360.com>
2021-11-02 16:55:45 +0800

    update: cdn

--- src/utils/cdn/index.js
+++ src/utils/cdn/index.js
@@ -2,33 +2,33 @@ module.exports.CDN = {
   develop: {
     js: [
       "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js",
-      "https://cdn.jsdelivr.net/npm/vue-router@3.0.3/dist/vue-router.min.js",
-      "https://cdn.jsdelivr.net/npm/vuex@3.1.2/dist/vuex.min.js",
-      "https://cdn.jsdelivr.net/npm/axios@0.19.2/dist/axios.min.js",
+      "https://cdn.jsdelivr.net/npm/vue-router@3.2.0/dist/vue-router.min.js",
+      "https://cdn.jsdelivr.net/npm/vuex@3.4.0/dist/vuex.min.js",
+      "https://cdn.jsdelivr.net/npm/axios@0.24.0/dist/axios.min.js",
     ],
   },
   test: {
     js: [
       "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js",
-      "https://cdn.jsdelivr.net/npm/vue-router@3.0.3/dist/vue-router.min.js",
-      "https://cdn.jsdelivr.net/npm/vuex@3.1.2/dist/vuex.min.js",
-      "https://cdn.jsdelivr.net/npm/axios@0.19.2/dist/axios.min.js",
+      "https://cdn.jsdelivr.net/npm/vue-router@3.2.0/dist/vue-router.min.js",
+      "https://cdn.jsdelivr.net/npm/vuex@3.4.0/dist/vuex.min.js",
+      "https://cdn.jsdelivr.net/npm/axios@0.24.0/dist/axios.min.js",
     ],
   },
   uat: {
     js: [
       "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js",
-      "https://cdn.jsdelivr.net/npm/vue-router@3.0.3/dist/vue-router.min.js",
-      "https://cdn.jsdelivr.net/npm/vuex@3.1.2/dist/vuex.min.js",
-      "https://cdn.jsdelivr.net/npm/axios@0.19.2/dist/axios.min.js",
+      "https://cdn.jsdelivr.net/npm/vue-router@3.2.0/dist/vue-router.min.js",
+      "https://cdn.jsdelivr.net/npm/vuex@3.4.0/dist/vuex.min.js",
+      "https://cdn.jsdelivr.net/npm/axios@0.24.0/dist/axios.min.js",
     ],
   },
   production: {
     js: [
       "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js",
-      "https://cdn.jsdelivr.net/npm/vue-router@3.0.3/dist/vue-router.min.js",
-      "https://cdn.jsdelivr.net/npm/vuex@3.1.2/dist/vuex.min.js",
-      "https://cdn.jsdelivr.net/npm/axios@0.19.2/dist/axios.min.js",
+      "https://cdn.jsdelivr.net/npm/vue-router@3.2.0/dist/vue-router.min.js",
+      "https://cdn.jsdelivr.net/npm/vuex@3.4.0/dist/vuex.min.js",
+      "https://cdn.jsdelivr.net/npm/axios@0.24.0/dist/axios.min.js",
     ],
   },
 };
