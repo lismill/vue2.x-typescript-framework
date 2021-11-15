9291ea12dabc5947a56d295727a2212766d38ea4
Lean <lean.li@71360.com>
2021-11-15 11:09:34 +0800

    feat: js-cookie

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,14 @@ import store from "@/store";
 import i18n from "@/utils/i18n";
 import "@/components/index";
 import "@/assets/styles/index.scss";
+import prototype from "@/utils/prototype";
 
 Vue.config.productionTip = false;
+
+/**
+ * 添加实例 property
+ */
+prototype.forEach((item: { key: string; value: any }) => (Vue.prototype[item["key"]] = item.value));
 
 new Vue({
   router,
---
+++ src/utils/prototype/index.ts
@@ -0,0 +1,8 @@
+import Cookies from "js-cookie";
+
+export default [
+  {
+    key: "$cookies",
+    value: Cookies,
+  },
+];
